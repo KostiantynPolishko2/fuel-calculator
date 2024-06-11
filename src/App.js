472e58c210c414857e0e9572ed7b66f121dc3fcd
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-11 14:06:45 +0300

    1 Created template of the first row <input>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import './fuel-calculator.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div class='App-header'>
+      <form>
+        <div class='title'>
+          <p>Стоимость поездки</p>
+        </div>
+        <div class='name'>
+          <label for='distance'>Расстояние</label>
+        </div> 
+        <div class='input-field'>
+          <input type='text' id='distance' name='distance_km' placeholder='введите километраж'></input>
+          <p>км</p>
+          <p class='note'>Note</p>
+        </div>
+      </form>
     </div>
+
   );
 }
 
