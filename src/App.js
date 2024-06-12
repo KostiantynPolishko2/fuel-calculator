a580cd6b8dd360ab7df0a3bed3349259692e1e38
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-12 18:18:02 +0300

    3 Set up algorithm for get inputted value.
    Done arrow functions isDigitValue(e), getDigitValue(e).
    
    isDigitValue(e) has pattern regex in order to make correct parseFloat().

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import './fuel-calculator.css';
 
 function App() {
 
-  let value = 10;
-  const [getValue, setValue] = useState(value);
-  const [getMargin, setMargin] = useState(0);
-  let p_element = <p class='note'>p tag element</p>;
+  // disabled={isDistance? 'disabled' : ''}
+  const [isDistance, setIsDistance] = useState(false);
+  let [distance, setDistance] = useState(0);
   
   return (
     <div class='App-header'>
-      <form>
+      <form id='fuel-calculation'>
         <div class='title'>
           <p>Стоимость поездки</p>
         </div>
@@ -19,56 +18,26 @@ function App() {
           <label for='distance'>Расстояние</label>
         </div> 
         <div class='input-field'>
-          <input type='text' id='distance' name='distance_km' placeholder='введите километраж'></input>
+          <input onBlur={(e) => {isDigitValue(e) ? setDistance(getDigitValue(e)) : setDistance(0)}} type='text' id='distance' name='distance_km' placeholder='введите километраж' autoFocus></input>
           <p>км</p>
           <p class='note'>Note</p>
         </div>
-      </form>
-      
-      {/* <button onClick={(e) => setValue(setNewValue(e, getValue))}>Calculate</button> */}
-
-      {/* <button onClick={(e) => setMargin(setMarginLeft(e, getMargin))}>Calculate</button>
-      <button onClick={showInfoEvent}>BtnEvent</button>
-      <button onClick={() => showInfoText('check call btn-info')}>BtnInfo</button>
-      <button onClick={(e) => showInfoEventText(e, 'check call btn__info-event')}>BtnInfoEvent</button> */}
-
-      <button onClick={() => setMargin(setMarginLeft(getMargin))}>BtnTest</button>
-      <p>{getText('Hello world!')}</p>
-      <p>Margin left: {getMargin != 0 ? getMargin : 'none'}</p>
-
-      <p>{`custom value ${value}`}</p>
-      <p style={{backgroundColor: 'red', marginLeft: getMargin + 'px'}}>New Position = {getMargin}</p>
-      {p_element}
-      
+        <div class='input-field' style={{marginTop: 15 + 'px'}}>
+          <button type='button' name='cost' form='fuel-calculation'>Рассчитать</button>
+          <p class='cost'>cost = {distance}</p>
+        </div>
+      </form>  
     </div>
-
   );
 }
 
-const getText = (text) => {
-  return `function text: ${text}`;
+const isDigitValue = (e) => {
+  const exp = /^\d+.?\d+$/;
+  return exp.test(e.target.value);
 }
 
-const setMarginLeft = (value) => {
-  return value += 50;
-}
-
-const setNewValue = (e, value) => {
-  console.log(e.target.textContent, `value = ${value}`);
-  
-  return value += 10;
-}
-
-const showInfoEvent = (e) => {
-  console.log(e.target.textContent);
-}
-
-const showInfoText = (text) => {
-  console.log('test', text);
-}
-
-const showInfoEventText = (e, text) => {
-  console.log(e.target.textContent, text);
+const getDigitValue = (e) => {
+  return parseFloat(e.target.value);
 }
 
 export default App;
