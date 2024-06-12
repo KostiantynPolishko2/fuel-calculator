d6087a6422f9a24d1c2650f82eb2b87ca022c229
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-12 21:28:50 +0300

    6 Done arrow function calcCost(distance, consume, price)
    Function assied to btn 'cost'.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,32 @@ import './fuel-calculator.css';
 function App() {
 
   // disabled={isDistance? 'disabled' : ''}
-  let [isDistance, setIsDistance] = useState(false);
-  let [distance, setDistance] = useState(0);
+  const [isDistance, setIsDistance] = useState(false);
+  const [distance, setDistance] = useState(0);
 
-  // const [isConsume, setIsConsume] = useState(false);
-  // const [consume, setConsume] = useState(0);
+  const [isConsume, setIsConsume] = useState(false);
+  const [consume, setConsume] = useState(0);
 
-  // const [isPrice, setIsPrice] = useState(false);
-  // const [price, setPrice] = useState(0);
+  const [isPrice, setIsPrice] = useState(false);
+  const [price, setPrice] = useState(0);
 
-  const setStates = (e) => {
-    if(isDigitValue(e)){
-      setIsDistance(true);
-      setDistance(getDigitValue(e));
-    }
-    else{
-      setIsDistance(false);
-      setDistance(0);
-    }
+  const [cost, setCost] = useState(0);
+
+  const setStateDistance = (e) => {
+    if(isDigitValue(e)){ setIsDistance(true); setDistance(getDigitValue(e));}
+    else{ setIsDistance(false); setDistance(0);}
   }
   
+  const setStateConsume = (e) => {
+    if(isDigitValue(e)){ setIsConsume(true); setConsume(getDigitValue(e));}
+    else{ setIsConsume(false); setConsume(0);}
+  }
+
+  const setStatePrice = (e) => {
+    if(isDigitValue(e)){ setIsPrice(true); setPrice(getDigitValue(e));}
+    else{ setIsPrice(false); setPrice(0);}
+  }
+
   return (
     <div class='App-header'>
       <form id='fuel-calculation'>
@@ -36,40 +42,44 @@ function App() {
           <label for='distance'>Расстояние</label>
         </div> 
         <div class='input-field'>
-          <input onBlur={setStates} type='text' id='distance' name='distance_km' placeholder='введите километраж' autoFocus></input>
-          <p>км {distance}</p>
+          <input onBlur={setStateDistance} type='text' id='distance' name='distance_km' placeholder='введите километраж' autoFocus></input>
+          <p>км</p>
           <p class='note'>{errorInfo(isDistance, distance)}</p>
         </div>
 
         {/* BLOCK AVERAGE CONSUMENION */}
-        {/* <div class='name'>
+        <div class='name'>
           <label for='consume'>Средний расход топлива</label>
         </div> 
         <div class='input-field'>
-          <input onBlur={(e) => {setIsConsume(isDigitValue(e)) ? setConsume(getDigitValue(e)) : setConsume(0)}} type='text' id='consume' name='consume_lkm' placeholder='введите рассход'></input>
+          <input onBlur={setStateConsume} type='text' id='consume' name='consume_lkm' placeholder='введите рассход' disabled={!isDistance? 'disabled' : ''}></input>
           <p>л./100км</p>
           <p class='note'>{errorInfo(isConsume, consume)}</p>
-        </div> */}
+        </div>
 
         {/* BLOCK PRICE */}
-        {/* <div class='name'>
+        <div class='name'>
           <label for='price'>Стоимость 1 л. топлива</label>
         </div> 
         <div class='input-field'>
-          <input onBlur={(e) => {setIsPrice(isDigitValue(e)) ? setPrice(getDigitValue(e)) : setPrice(0)}} type='text' id='price' name='price_uah' placeholder='введите цену'></input>
+          <input onBlur={setStatePrice} type='text' id='price' name='price_uah' placeholder='введите цену' disabled={!isConsume? 'disabled' : ''}></input>
           <p>грн/літр</p>
           <p class='note'>{errorInfo(isPrice, price)}</p>
-        </div> */}
+        </div>
 
         {/* BUTTON */}
         <div class='input-field' style={{marginTop: 25 + 'px'}}>
-          <button type='button' name='cost' form='fuel-calculation'>Рассчитать</button>
-          <p class='cost' style={{width: 50 + '%'}}>UAH</p>
+          <button onClick={() => setCost(calcCost(distance, consume, price))} type='button' name='cost' form='fuel-calculation' className={isPrice? '' : 'disabled'}>Рассчитать</button>
+          <p class='cost' style={{width: 50 + '%'}}>{cost ? `${cost} UAH` : ''}</p>
         </div>
 
       </form>  
     </div>
   );
+}
+
+const calcCost = (distance, consume, price) => {
+  return ((distance / 100) * consume * price).toFixed(2);
 }
 
 const errorInfo = (flag, value) => {
@@ -82,13 +92,10 @@ const errorInfo = (flag, value) => {
 
 const isDigitValue = (e) => {
   const exp = /^\d+.?\d+$/;
-  console.log(exp.test(e.target.value));
-
   return exp.test(e.target.value);
 }
 
 const getDigitValue = (e) => {
-  console.log(parseFloat(e.target.value));
   return parseFloat(e.target.value);
 }
 
